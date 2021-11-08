3ebd807ffa02de9455f37d2b7fb1a134731a79b8
Diogo Falcão <73653498+straveski@users.noreply.github.com>
2021-11-08 19:46:36 +0000

    Add files via upload

--- project/ggc-app/src/ggc/app/transactions/DoRegisterAcquisitionTransaction.java
+++ project/ggc-app/src/ggc/app/transactions/DoRegisterAcquisitionTransaction.java
@@ -13,11 +13,17 @@ public class DoRegisterAcquisitionTransaction extends Command<WarehouseManager> 
   public DoRegisterAcquisitionTransaction(WarehouseManager receiver) {
     super(Label.REGISTER_ACQUISITION_TRANSACTION, receiver);
     //FIXME maybe add command fields
+    addStringField("partnerID", Prompt.partnerKey());
+    addStringField("prodID", Prompt.productKey());
+    addRealField("price", Prompt.price());
+    addIntegerField("quantity", Prompt.amount());
   }
 
   @Override
   public final void execute() throws CommandException {
     //FIXME implement command
+    
+      _receiver.registerAcquisition(stringField("partnerID"), stringField("prodID"), realField("price"), integerField("quantity"));
+    
   }
-
 }
