3e893b1e824ef8c36078f1eb4f211ea8ca51a502
Diogo Falcão <73653498+straveski@users.noreply.github.com>
2021-11-09 22:49:55 +0000

    Add files via upload

--- project/ggc-app/src/ggc/app/transactions/DoShowTransaction.java
+++ project/ggc-app/src/ggc/app/transactions/DoShowTransaction.java
@@ -3,6 +3,8 @@ package ggc.app.transactions;
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
 import ggc.WarehouseManager;
+import ggc.app.exceptions.UnknownTransactionKeyException;
+import ggc.exceptions.UnknownTransactionIDException;
 //FIXME import classes
 
 /**
@@ -13,11 +15,17 @@ public class DoShowTransaction extends Command<WarehouseManager> {
   public DoShowTransaction(WarehouseManager receiver) {
     super(Label.SHOW_TRANSACTION, receiver);
     //FIXME maybe add command fields
+    addIntegerField("transactionID", Prompt.transactionKey());
   }
 
   @Override
   public final void execute() throws CommandException {
     //FIXME implement command
+    if (!_receiver.transIdExists(integerField("transactionID")))
+      throw new UnknownTransactionKeyException(integerField("transactionID"));
+    else
+      _display.add(_receiver.showTransaction(integerField("transactionID")));
+    _display.display();
   }
-
+  
 }
