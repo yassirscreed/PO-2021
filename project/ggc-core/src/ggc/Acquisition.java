b6cce1797ba64911fa1ef383f7c91f2eb8a17f13
Diogo Falcão <73653498+straveski@users.noreply.github.com>
2021-11-11 21:12:14 +0000

    Add files via upload

--- project/ggc-core/src/ggc/Acquisition.java
+++ project/ggc-core/src/ggc/Acquisition.java
@@ -7,8 +7,8 @@ public class Acquisition extends Transaction{
 
     private double _payvalue;
 
-    public Acquisition(int id, String partnerID, String productID, int quantity, int paydate, double payvalue){
-        super(id, partnerID, productID, quantity, paydate);
+    public Acquisition(int id, String partnerID, String productID, int quantity, int paydate, String type, double payvalue){
+        super(id, partnerID, productID, quantity, paydate, type);
         _payvalue = payvalue;
     }
 
@@ -19,9 +19,10 @@ public class Acquisition extends Transaction{
     public void setPayValue(int payvalue){
         _payvalue = payvalue;
     }
+
     
     @Override
     public String toString(){
-        return "COMPRA|" + getId() + "|" + getPartnerID() + "|" + getProductID() + "|" + getQuantity() + "|" + (int)Math.round(getPayValue()) + "|" + getPayDate();
+        return "COMPRA|" + getId() + "|" + getPartnerID() + "|" + getProductID() + "|" + getQuantity() + "|" + (int)Math.round(getPayValue()) + "|" + getDate();
     }
 }
