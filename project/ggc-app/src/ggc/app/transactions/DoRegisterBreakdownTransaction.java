10331e2749f11c0bdcd4dae898f412cf20841525
Diogo Falcão <73653498+straveski@users.noreply.github.com>
2021-11-11 21:10:47 +0000

    Add files via upload

--- project/ggc-app/src/ggc/app/transactions/DoRegisterBreakdownTransaction.java
+++ project/ggc-app/src/ggc/app/transactions/DoRegisterBreakdownTransaction.java
@@ -4,6 +4,13 @@ import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
 import ggc.WarehouseManager;
 //FIXME import classes
+import ggc.app.exceptions.UnknownPartnerKeyException;
+import ggc.exceptions.UnknownPartnerIDException;
+import ggc.app.exceptions.UnknownProductKeyException;
+import ggc.exceptions.UnknownProductIDException;
+import ggc.app.exceptions.UnavailableProductException;
+import ggc.exceptions.UnavailableProductCoreException;
+import pt.tecnico.uilib.forms.*;
 
 /**
  * Register order.
@@ -13,11 +20,26 @@ public class DoRegisterBreakdownTransaction extends Command<WarehouseManager> {
   public DoRegisterBreakdownTransaction(WarehouseManager receiver) {
     super(Label.REGISTER_BREAKDOWN_TRANSACTION, receiver);
     //FIXME maybe add command fields
+    addStringField("partnerID", Prompt.partnerKey());
+    addStringField("prodID", Prompt.productKey());
+    addIntegerField("quantity", Prompt.amount());
   }
 
   @Override
   public final void execute() throws CommandException {
     //FIXME implement command
-  }
+    String partnerID = stringField("partnerID");
+    String prodID = stringField("prodID");
+    int quantity = integerField("quantity");
+    try{
+      _receiver.registerBreakdown(partnerID,prodID,quantity);
+    } catch (UnknownPartnerIDException e){
+      throw new UnknownPartnerKeyException(e.getKey());
+    } catch (UnknownProductIDException e){
+      throw new UnknownProductKeyException(e.getKey());
+    } catch (UnavailableProductCoreException e){
+      throw new UnavailableProductException(e.getKey(), e.getRequested(), e.getAvailable());
+    }
 
+  }
 }
