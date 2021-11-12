ddb6508160de099f26be5e4f94bdd448d0bb8a96
Diogo Falcão <73653498+straveski@users.noreply.github.com>
2021-11-12 07:43:28 +0000

    Add files via upload

--- project/ggc-app/src/ggc/app/partners/DoShowPartnerSales.java
+++ project/ggc-app/src/ggc/app/partners/DoShowPartnerSales.java
@@ -4,6 +4,8 @@ import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
 import ggc.WarehouseManager;
 //FIXME import classes
+import ggc.app.exceptions.UnknownPartnerKeyException;
+import ggc.exceptions.UnknownPartnerIDException;
 
 /**
  * Show all transactions for a specific partner.
@@ -13,11 +15,18 @@ class DoShowPartnerSales extends Command<WarehouseManager> {
   DoShowPartnerSales(WarehouseManager receiver) {
     super(Label.SHOW_PARTNER_SALES, receiver);
     //FIXME add command fields
+    addStringField("id", Prompt.partnerKey());
   }
 
   @Override
   public void execute() throws CommandException {
     //FIXME implement command
+    try {
+      _display.add(_receiver.showPartnerSales(stringField("id")));
+      _display.display();
+    } catch (UnknownPartnerIDException e){
+      throw new UnknownPartnerKeyException(e.getKey());
+    }
   }
 
 }
