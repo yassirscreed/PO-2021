602595e5d0133c67a36cb84dbbd9cdc20c733c1c
Diogo Falcão <73653498+straveski@users.noreply.github.com>
2021-11-12 07:46:12 +0000

    Add files via upload

--- project/ggc-app/src/ggc/app/transactions/DoReceivePayment.java
+++ project/ggc-app/src/ggc/app/transactions/DoReceivePayment.java
@@ -3,7 +3,9 @@ package ggc.app.transactions;
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
 import ggc.WarehouseManager;
-//FIXME import classes
+//FIXME import classess
+import ggc.app.exceptions.UnknownTransactionKeyException;
+import ggc.exceptions.UnknownTransactionIDException;
 
 /**
  * Receive payment for sale transaction.
@@ -13,11 +15,16 @@ public class DoReceivePayment extends Command<WarehouseManager> {
   public DoReceivePayment(WarehouseManager receiver) {
     super(Label.RECEIVE_PAYMENT, receiver);
     //FIXME add command fields
+    addIntegerField("transID", Prompt.transactionKey());
   }
 
   @Override
   public final void execute() throws CommandException {
     //FIXME implement command
+    try {
+      _receiver.receivePayment(integerField("transID"));
+    } catch (UnknownTransactionIDException e) {
+      throw new UnknownTransactionKeyException(e.getKey());
+    }
   }
-
 }
